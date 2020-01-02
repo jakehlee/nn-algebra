aea4195f1e4f46f9d64f2e07155bbf3ee3fa0dfa
Jake Lee <jakehlee1@gmail.com>
2020-01-02 00:31:17 -0800

    Updated Shortout implementation

--- shortout/torch_shortout.py
+++ shortout/torch_shortout.py
@@ -1,10 +1,11 @@
 import torch
+import torch.nn as nn
 
 # referencing "PyTorch: Custom nn Modules"
 # https://pytorch.org/tutorials/beginner/pytorch_with_examples.html#pytorch-custom-nn-modules
 # https://pytorch.org/docs/stable/_modules/torch/nn/modules/dropout.html#Dropout
 
-class ConvShortout(torch.nn.Module):
+class ConvShortout(nn.Module):
 
     def __init__(self, p=0.01):
         # p here is the short probability
@@ -15,12 +16,13 @@ class ConvShortout(torch.nn.Module):
 
     def forward(self, X):
         if self.training:
-            if p == 0:
+            if self.p == 0:
                 return X
             # I'm assuming that X is (N, C, H, W).
 
             # 1. Maxpool the entire channel
             maxes = torch.nn.functional.max_pool2d(X, kernel_size=X.size()[2:])
+            #maxes = nn.AdaptiveMaxPool2d((1,1))
             # maxes will be (N, C, 1, 1)
             maxes2 = torch.cat([maxes] * X.size(2), 2)
             maxes3 = torch.cat([maxes] * X.size(3), 3)
@@ -29,7 +31,10 @@ class ConvShortout(torch.nn.Module):
             # 2. Dropout the entire thing
             # We're going to implement this from scratch here
             dist = torch.distributions.binomial.Binomial(probs=1-self.p)
-            mask = dist.sample(X.size()).cuda()
+            if X.is_cuda:
+                mask = dist.sample(X.size()).cuda()
+            else:
+                mask = dist.sample(X.size())
             #mask = dist.sample(X.size())
             # mask will be (N, C, H, W)
 
@@ -37,4 +42,3 @@ class ConvShortout(torch.nn.Module):
             # no rescaling for now...
             shorted = mask * X + (1-mask) * maxes3
             return shorted
-            
